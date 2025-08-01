2f38634c350f7e41f5dae78679232911d95a24cd
RaynaFD <farr0254@algonquinlive.com>
2025-08-01 18:59:05 -0400

    connections made and posts retrieved

--- BogBlogPlus/src/App.jsx
+++ BogBlogPlus/src/App.jsx
@@ -2,11 +2,22 @@ import {
     createBrowserRouter, RouterProvider
 } from "react-router";
 import './App.css'
-import Home from "./pages/Home.jsx";
+import Home, {postsLoader}from "./pages/Home.jsx";
+
+//make pages for main blog pages (all posts), blog post (singular), login, home, loading, error
+// single user blog will be main blog page with just users posts
+// create header/nav bar for top and footer bar for bottom of each page - included in layout
+//add routing 
+//study api call readme for how to get data
+
 
 function App() {
     const router = createBrowserRouter([
-        {path: "/", element: <Home/>}
+
+        {path: "/",
+         element: <Home/>,
+         loader: postsLoader
+        }
     ])
 
         return (
--- BogBlogPlus/src/pages/Home.jsx
+++ BogBlogPlus/src/pages/Home.jsx
@@ -1,7 +1,44 @@
+import {useLoaderData} from 'react-router'
+
+function BlogPosts(){
+    const posts = useLoaderData();
+    
+    return(
+        <div className="container">
+       
+        {posts.map(post =>  <div className="card"><h3>{post.title}</h3><p key={post.title}>{post.text}</p></div>)}
+        
+        </div>
+    )
+}
+
 export default function Home() {
     return (
         <>
             <h1>Home</h1>
+           <BlogPosts />
         </>
     )
 }
+
+//fetch and load data
+export async function postsLoader() {
+    const token = localStorage.getItem("token");
+  
+    const response = await fetch("http://127.0.0.1:5055/api/post", {
+      headers: {
+        Authorization: `Bearer ${token}`,
+      },
+    });
+  
+    const data = await response.json(); 
+  
+    if (!response.ok) {
+      console.error("Fetch failed:", data);
+      return [];
+    }
+  
+    console.log("Posts fetched:", data);
+    return data.posts;
+  }
+  
