ce023511627ca82308ad0f3693b06d9ac7df432c
RaynaFD <farr0254@algonquinlive.com>
2025-08-04 23:31:23 -0400

    navbar responsive

--- BogBlogPlus/src/components/NavBar.jsx
+++ BogBlogPlus/src/components/NavBar.jsx
@@ -1,9 +1,12 @@
 import { Link } from "react-router-dom";
 import { tokenLogout } from "../utils/api";
+import {Bars3Icon} from "@heroicons/react/16/solid"
+import { useState } from "react";
 
 export default function NavBar({title}) {
   const token = localStorage.getItem("token");
   const id = localStorage.getItem("userId");
+  const [menuOpen, setMenuOpen] = useState(false);
 
   const handleLogout = () => {
         void tokenLogout(token);
@@ -18,21 +21,26 @@ export default function NavBar({title}) {
         <div className="titleLink"><Link to="/" ><img className="navBarLogo" src={"/src/assets/SVG/Logo.svg"}/></Link>
     <h1 className="navbar-title">{title}</h1></div>
 
-    <div className="navbar-links">
-      <Link to="/">Home</Link> &nbsp; | &nbsp;
-      {token && (
-      <>
-        <Link to= {`/user/${id}/posts`}>My Posts</Link> &nbsp; | &nbsp;
-        <Link to="/new">New Post</Link> &nbsp; | &nbsp;
-        <Link to={`/user/${id}/account`}>My Account</Link> &nbsp; | &nbsp;
-        <button onClick={handleLogout}>Logout</button>
-        </>)}
-      {!token &&(
-        <>
-          <Link to="/login">Login</Link> &nbsp; | &nbsp;
-          <Link to="/register">Register</Link>
-        </>)}
-      
+    <button className="hamburger" onClick={() => setMenuOpen(!menuOpen)}>
+        <Bars3Icon className="bar-icon" id="barButton"/>
+      </button>
+
+    <div className={`navbar-links ${menuOpen ? "show" : ""}`}>
+     
+      {token ? (
+      <> <ul className="navbarList">
+        <li><Link to="/">Home</Link> </li> 
+      <li><Link to= {`/user/${id}/posts`}>My Posts</Link> </li>  
+       <li><Link to="/new">New Post</Link> </li> 
+       <li><Link to={`/user/${id}/account`}>My Account</Link></li> </ul>
+        <button className="barButtonLogout" onClick={handleLogout}>Logout</button>
+        </>):(
+        <><ul className="navbarList">
+        <li><Link to="/">Home</Link> </li> 
+         <li><Link to="/login">Login</Link></li> 
+         <li> <Link to="/register">Register</Link></li>
+       </ul>  </>)}
+     
     </div>
   </nav>
 );
